a88c726152f3571c6a294d8275329cc867a14d01
Nádia Mizael <nadia.mizael@gmail.com>
2022-05-11 11:37:10 -0300

    ✨ feat: add router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/pages/Home";
+import Company from "./components/pages/Company";
+import Contact from "./components/pages/Contact";
+import NewProject from "./components/pages/NewProject";
+
 function App() {
   return (
-    <div className="App">
-      <p> Costs </p>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/company" element={<Company />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/newproject" element={<NewProject />} />
+      </Routes>
+    </Router>
   );
 }
 
