2fc3607c5b9fa9ab40a9fe4f0934adf1e33a8935
Nádia Mizael <nadia.mizael@gmail.com>
2022-05-16 12:34:03 -0300

    ✨ feat: add input component

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,14 +1,21 @@
+import Input from "../form/Input";
 import styles from "./ProjectForm.module.css";
 
 function ProjectForm() {
   return (
     <form className={styles.form}>
-      <div>
-        <input type="text" placeholder="Insira o nome do projeto" />
-      </div>
-      <div>
-        <input type="number" placeholder="Insira o orçamento total" />
-      </div>
+      <Input
+        type="text"
+        text="Nome do projeto"
+        name="name"
+        placeholder="Insira o nome do projeto"
+      />
+      <Input
+        type="number"
+        text="Orçamento do projeto"
+        name="budget"
+        placeholder="Insira o orçamento total"
+      />
 
       <div>
         <select name="category_id">
